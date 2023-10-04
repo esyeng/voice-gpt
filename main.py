8795c76e34203f33aecf9ed02283b0210d9765be
Esme Y <61486616+esyeng@users.noreply.github.com>
2023-10-03 21:13:45 -0400

    Working interface status

--- main.py
+++ main.py
@@ -1,14 +1,89 @@
-from dotenv import load_dotenv
 import openai
-import os
+import playsound
+import time
+import threading
+import speech_recognition as sr
+import logging
+import traceback
+from gtts import gTTS
+from initialize import init_openai
 
 
-def init_openai():
-    load_dotenv()
-    openai.api_key = os.getenv("API_KEY")
-    print("Key loaded.")
+
+logging.basicConfig(level=logging.ERROR)  # Configures logging to show errors
+lang = "en"
+
+init_openai()
+
+# Flag to signal when the text is currently being spoken
+is_speaking = threading.Event()
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    init_openai()
+def speak(text: str):
+    """
+    Speak the given text using gTTS (Google Text-to-Speech).
+
+    Args:
+    - text (str): The text to be spoken.
+
+    Returns:
+    - None
+    """
+    is_speaking.set()  # Signal that the program is speaking
+    speech = gTTS(text=text, lang=lang, slow=False, tld="ie")
+    speech.save("output.mp3")
+    playsound.playsound("output.mp3")  # Play the audio
+    time.sleep(len(text) * 0.06)  # Rough estimation to ensure the event clears after the text is spoken
+    is_speaking.clear()  # Reset the speaking flag
+
+
+def get_audio():
+    r = sr.Recognizer()
+    with sr.Microphone(device_index=1) as source:
+        print("Listening...")
+        try:
+            audio = r.listen(source)
+        except sr.UnknownValueError:
+            logging.error("Google Speech Recognition could not understand audio")
+            return ""
+        except sr.RequestError as e:
+            logging.error("Could not request results from Google Speech Recognition service; {0}".format(e))
+            return ""
+
+        said = ""
+        try:
+            said = r.recognize_google(audio).lower()
+            print("You said", said)
+
+            if "" in said:
+                speak("Yes Goddess?")
+                messages = [
+                    {"role": "system", "content": "You are a sassy assistant"},
+                    {"role": "user", "content": said}
+                ]
+                completion = openai.ChatCompletion.create(
+                    model="gpt-3.5-turbo-0613",
+                    messages=messages
+                )
+                print(completion)
+                speak(completion.choices[0].message.content)
+
+            if "stop" in said:
+                speak("Okay.")
+                return "stop"
+
+        except sr.UnknownValueError:
+            logging.error("Google Speech Recognition could not understand audio")
+        except sr.RequestError as e:
+            logging.error("Could not request results from Google Speech Recognition service; {0}".format(e))
+        except Exception as e:
+            logging.error("An unexpected error occurred: {0}".format(repr(e)))
+            logging.error(traceback.format_exc())  # This will print the entire stack trace
+
+    return said
+
+while True:
+    result = get_audio()
+    if result == "stop":
+        break
+
